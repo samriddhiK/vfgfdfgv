e6e6554249aed8f07c1c01a5d56d25999a886a88
samriddhiK <75959569+samriddhiK@users.noreply.github.com>
2021-07-10 11:35:01 -0700

    Add files via upload

---
+++ Student_Driving_license.py
@@ -0,0 +1,27 @@
+print("put blade back in jailbreak plssssssssssssssssss")
+a =100
+print(type(a))
+
+city ='los Angeles'
+print(type(city))
+
+t = str (4) + str (3)
+print(t)
+
+blades_friends = ["volt bike " "monster truck " "black hawnk " "jet and " "sloop"]
+print(blades_friends)
+print(0)
+
+blades_replacment = ["drone"]
+print(blades_replacment)
+print(1)
+
+from tkinter import *
+
+root=Tk()
+root.title("add blade back")
+root.geometry("400x400")
+              
+root.mainloop()
+
+root.text("blade is the bestttttttttttttt")
